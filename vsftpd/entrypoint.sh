7372c670be28ad1099df91ce27771d39f42a3c66
Ameya Shenoy <shenoy.ameya@gmail.com>
2020-01-04 00:29:57 +0530

    feat: add vsftpd
    Signed-off-by: Ameya Shenoy <shenoy.ameya@gmail.com>

---
+++ vsftpd/entrypoint.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Create home dir and update vsftpd user db:
+mkdir -p "/home/vsftpd/${FTP_USER}"
+chown -R ftp:ftp /home/vsftpd/
+
+echo -e "${FTP_USER}\n${FTP_PASS}" > /etc/vsftpd/virtual_users.txt
+/usr/bin/db_load -T -t hash -f /etc/vsftpd/virtual_users.txt /etc/vsftpd/virtual_users.db
+
+# Set passive mode parameters:
+if [ "$PASV_ADDRESS" = "**IPv4**" ]; then
+    export PASV_ADDRESS=$(/sbin/ip route|awk '/default/ { print $3 }')
+fi
+
+echo "pasv_address=${PASV_ADDRESS}" >> /etc/vsftpd/vsftpd.conf
+echo "pasv_max_port=${PASV_MAX_PORT}" >> /etc/vsftpd/vsftpd.conf
+echo "pasv_min_port=${PASV_MIN_PORT}" >> /etc/vsftpd/vsftpd.conf
+echo "pasv_addr_resolve=${PASV_ADDR_RESOLVE}" >> /etc/vsftpd/vsftpd.conf
+echo "pasv_enable=${PASV_ENABLE}" >> /etc/vsftpd/vsftpd.conf
+echo "file_open_mode=${FILE_OPEN_MODE}" >> /etc/vsftpd/vsftpd.conf
+echo "local_umask=${LOCAL_UMASK}" >> /etc/vsftpd/vsftpd.conf
+echo "xferlog_std_format=${XFERLOG_STD_FORMAT}" >> /etc/vsftpd/vsftpd.conf
+
+# Get log file path
+export LOG_FILE=`grep xferlog_file /etc/vsftpd/vsftpd.conf|cut -d= -f2`
+
+# stdout server info:
+if [ ! $LOG_STDOUT ]; then
+cat << EOB
+	*************************************************
+	*                                               *
+	*    Docker image: fauria/vsftd                 *
+	*    https://github.com/fauria/docker-vsftpd    *
+	*                                               *
+	*************************************************
+	SERVER SETTINGS
+	---------------
+	· FTP User: $FTP_USER
+	· FTP Password: $FTP_PASS
+	· Log file: $LOG_FILE
+	· Redirect vsftpd log to STDOUT: No.
+EOB
+else
+    /usr/bin/ln -sf /dev/stdout $LOG_FILE
+fi
+
+# Run vsftpd:
+&>/dev/null /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
